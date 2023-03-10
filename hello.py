06035e981cb8a2384eb93e9be19d44ff40c900e1
Hassaan Memon <hassaanmemonduke@gmail.com>
2023-03-10 22:47:48 +1100

    initial commit

---
+++ hello.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+#create a route decorator
+@app.route('/')
+
+def index():
+    first_name = "John"
+    favourites = ["burger", "pizza"]
+    return render_template("index.html", first_name=first_name, favourites=favourites)
+
+@app.route('/user/<name>')
+
+def user(name):
+    return render_template("user.html", user_name=name)
+
+
+#invalid url
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+#invalid url
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
